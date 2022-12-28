61210181eb6f0d2c82729297c8125270f05ad857
samilao101 <delacruz101@gmail.com>
2022-12-28 12:12:05 -0500

    Initial Commit

---
+++ ResearchAI/ResearchAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchAIApp.swift
+//  ResearchAI
+//
+//  Created by Sam Santos on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ResearchAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
