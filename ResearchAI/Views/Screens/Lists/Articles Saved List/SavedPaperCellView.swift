289da8921be415eb48195b901cb8d643679e20ce
samilao101 <delacruz101@gmail.com>
2023-04-29 06:39:07 -0400

    working on list of saved paper

---
+++ ResearchAI/Views/Screens/Lists/Articles Saved List/SavedPaperCellView.swift
@@ -0,0 +1,20 @@
+//
+//  SavedPaperCellView.swift
+//  ResearchAI
+//
+//  Created by Sam Santos on 4/29/23.
+//
+
+import SwiftUI
+
+struct SavedPaperCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SavedPaperCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        SavedPaperCellView()
+    }
+}
